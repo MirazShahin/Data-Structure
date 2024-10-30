f1a1299b2afefb8ea908218bd51b9a2772681597
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-10-31 03:17:50 +0600

    Update Range Update Queries.cpp

--- Segment tree/Lazy Propagation/Range Update Queries.cpp
+++ Segment tree/Lazy Propagation/Range Update Queries.cpp
@@ -1,93 +1,113 @@
 #include <bits/stdc++.h>
+
 #define FastIO                  \
   ios_base::sync_with_stdio(0); \
   cin.tie(0);                   \
-  cout.tie(0); 
+  cout.tie(0);
 
 using namespace std;
-using ll = long long int;
-const int N = 2e5 + 5;
 
-ll tree[N * 4], lazy[N * 4];
+const int N = 2e5;
+
+int n, q;
 int arr[N];
-void build(int node, int st, int en) {
-   if(st == en) {
-      tree[node] = arr[st];
-      return;
-   }
-   int mid = (st + en) / 2;
-   build(2 * node, st, mid);
-   build(2 * node + 1, mid + 1, en);
-   tree[node] = tree[2 * node] + tree[2 * node + 1];
-}
 
-void propagate(int node, int st, int en) {
-    if (lazy[node] != 0) { 
-        tree[node] += (en - st + 1) * lazy[node];
-        if (st != en) {
-            lazy[2 * node] += lazy[node];
-            lazy[2 * node + 1] += lazy[node];
+struct stree {
+    long long tree[4 * N], lazy[4 * N];  
+
+    void push(int node, int l, int r) {
+        if (lazy[node] == 0) {
+            return;
         }
-        lazy[node] = 0;
+        tree[node] = lazy[node] * (r - l + 1);
+        if (l != r) {
+            lazy[2 * node] = lazy[node];
+            lazy[2 * node + 1] = lazy[node];
+        }
+        lazy[node] = 0; 
     }
-}
-void lazy_update(int node, int st, int en, int l, int r, ll val) {
-    propagate(node, st, en); // Apply any pending updates
 
-    if (st > r or en < l) return; // No overlap
+    void build(int node, int l, int r) {
+        lazy[node] = 0;  
+        if (l == r) {
+            tree[node] = arr[l];  
+            return;
+        }
 
-    if (st >= l and en <= r) { // Full overlap
-        lazy[node] += val;
-        propagate(node, st, en);
-        return;
+        int mid = (l + r) >> 1;
+        build(2 * node, l, mid);
+        build(2 * node + 1, mid + 1, r);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
     }
- 
-    int mid = (st + en) / 2;
-    lazy_update(2 * node, st, mid, l, r, val);
-    lazy_update(2 * node + 1, mid + 1, en, l, r, val);
-    tree[node] = tree[2 * node] + tree[2 * node + 1];  
-}
-ll query(int node, int st, int en, int idx) {
-    propagate(node, st, en); 
-    if (st == en) return tree[node]; 
-    int mid = (st + en) / 2;
-    if (idx <= mid) return query(2 * node, st, mid, idx);
-    else return query(2 * node + 1, mid + 1, en, idx);
-}
+
+    void update(int node, int l, int r, int i, int j, int v) {
+        push(node, l, r);
+        if (r < i || j < l) {
+            return;  
+        }
+        if (i <= l && r <= j) {
+            lazy[node] = v;  
+            push(node, l, r);
+            return;  
+        }
+        int mid = (l + r) >> 1;
+        update(2 * node, l, mid, i, j, v);
+        update(2 * node + 1, mid + 1, r, i, j, v);
+        tree[node] = tree[2 * node] + tree[2 * node + 1]; 
+    }
+
+    long long query(int node, int l, int r, int i, int j) {
+        push(node, l, r);
+        if (r < i || j < l) {
+            return 0; 
+        }
+        if (i <= l && r <= j) {
+            return tree[node]; 
+        }
+        int mid = (l + r) >> 1;
+        long long x = query(2 * node, l, mid, i, j);
+        long long y = query(2 * node + 1, mid + 1, r, i, j);
+        return x + y;  
+    }
+};
+
+stree st;
+
 void solve() {
-    int n, q; cin >> n >> q;
-    for(int i = 0; i < n; i++ ) {
-        cin >> arr[i];
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
     }
-    build(1, 0, n - 1);
-    while(q-- ) {
-        int tp, l, r, val;  
+    st.build(1, 0, n - 1);
+    
+    while (q--) {
+        int tp; 
         cin >> tp;
-        if(tp == 2) {
-            cin >> l;
-            l--;
-            ll ans = query(1, 0, n - 1, l);
-            cout << ans << "\n";
-        }
+        if (tp == 1) {
+            int idx, val; 
+            cin >> idx >> val;
+            st.update(1, 0, n - 1, idx - 1, idx - 1, val);  
+        } 
         else {
-            cin >> l >> r >> val;
-            l--;
-            r--;
-            lazy_update(1, 0, n - 1, l, r, val);
+            int x; 
+            cin >> x;
+            st.update(1, 0, n - 1, 0, n - 1, x); 
         }
+        cout << st.query(1, 0, n - 1, 0, n - 1) << '\n';  
     }
-} 
+}
+
 int32_t main() {
     FastIO;
- 
+
     int test_case = 1;
-    // cin >> test_case;
+    // cin >> test_case; 
     int Case = 1;
-    while(test_case--) {
-//    cout << "Case " << Case << ": ";
+    while (test_case--) {
+        // cout << "Case " << Case << ": ";
         solve();
         Case++;
     }
 
     return 0;
-} 
+}
